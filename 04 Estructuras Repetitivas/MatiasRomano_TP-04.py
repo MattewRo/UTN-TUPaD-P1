8752ebab2a08116a5e2b1ff867ec580f3b2a33ed
Matias Antonio Romano <matiasantonioromano@gmail.com>
2025-04-18 19:58:34 -0300

    Ejercicio 2 hecho

--- 04 Estructuras Repetitivas/MatiasRomano_TP-04.py
+++ 04 Estructuras Repetitivas/MatiasRomano_TP-04.py
@@ -2,9 +2,18 @@
 (incluyendo ambos extremos), en orden creciente, mostrando un número por línea"""
 
 
-for i in range(0,101):
-    print(f"{i}")
+for i1 in range(0,101):
+    print(f"{i1}")
 
 """2) Desarrolla un programa que solicite al usuario un número entero y determine la cantidad de
 dígitos que contiene."""
 
+num = int(input("Ingrese su numero: "))
+i2 = 0
+while num > 0:
+    num = num // 10
+    i2 += 1
+print(f"El numero tiene {i2} digitos")
+
+"""3) Escribe un programa que sume todos los números enteros comprendidos entre dos valores
+dados por el usuario, excluyendo esos dos valores."""
