47e5af671b905f4dfb5b662a71e85899c78e80ef
Matias Antonio Romano <matiasantonioromano@gmail.com>
2025-04-18 19:42:59 -0300

    Ejercicio 1 hecho

---
+++ 04 Estructuras Repetitivas/MatiasRomano_TP-04.py
@@ -0,0 +1,10 @@
+"""1) Crea un programa que imprima en pantalla todos los números enteros desde 0 hasta 100
+(incluyendo ambos extremos), en orden creciente, mostrando un número por línea"""
+
+
+for i in range(0,101):
+    print(f"{i}")
+
+"""2) Desarrolla un programa que solicite al usuario un número entero y determine la cantidad de
+dígitos que contiene."""
+
