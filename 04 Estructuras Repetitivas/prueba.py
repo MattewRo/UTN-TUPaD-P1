53ba062715875e5f4aec3ca46be175835699d8d1
Matias Antonio Romano <matiasantonioromano@gmail.com>
2025-04-19 17:02:21 -0300

    Tp terminado y eliminacion de prueba.py

--- 04 Estructuras Repetitivas/prueba.py
+++
@@ -1,7 +0,0 @@
-numm = int(input("Ingresa el numero:"))
-numm_rev = 0
-while numm != 0:
-    digito = numm % 10
-    numm_rev = numm_rev * 10 + digito
-    numm = numm // 10
-print(f"El numero invertido de {numm} es {numm_rev}") 
