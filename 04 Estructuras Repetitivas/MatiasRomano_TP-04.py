ec6a72dc1ddbeec04f9e98d66be2f17d7fa0b249
Matias Antonio Romano <matiasantonioromano@gmail.com>
2025-04-18 23:03:21 -0300

    Empece ejercicio 9

--- 04 Estructuras Repetitivas/MatiasRomano_TP-04.py
+++ 04 Estructuras Repetitivas/MatiasRomano_TP-04.py
@@ -89,3 +89,6 @@ print(f"La cantidad de numeros pares es {num_par}, los numeros impares fueron {n
 
 """9) Elabora un programa que permita al usuario ingresar 100 números enteros y luego calcule la media de esos valores. (Nota: puedes probar el programa con una cantidad menor, pero debe
 poder procesar 100 números cambiando solo un valor)."""
+
+for i in range (0, 100):
+    
